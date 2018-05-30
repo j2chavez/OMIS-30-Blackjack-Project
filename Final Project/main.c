50ba3edbf8d5d4880c77f5d8f2a76911e730bdd5
John Chavez <John@johns-macbook.local>
2018-05-30 15:44:58 -0700

    Initial Commit

---
+++ Final Project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Final Project
+//
+//  Created by John Chavez on 5/30/18.
+//  Copyright © 2018 John Chavez. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
